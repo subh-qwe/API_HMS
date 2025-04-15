e35e71754ac7cb44e4cd3673f1eb653cd4c35055
Aman Singh <aman.singh@corp.csmpl.com>
2025-04-15 16:13:48 +0530

    Implemented JWT Auth

--- app/Models/UserRegistration.php
+++ app/Models/UserRegistration.php
@@ -2,12 +2,51 @@
 
 namespace App\Models;
 
+use Hash;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserRegistration extends Model
+class UserRegistration extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
+   use Authenticatable, Authorizable;
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
    protected $table = 'users';
    protected $fillable = ['name', 'email', 'password', 'phone_number', 'profile_image', 'role','remember_token','otp'];
 
    public $timestamps = true;
+
+   public function findUserByEmail($email){
+      return $this->where('email', $email)->first();
+   }
+   public function validPass($email, $password)
+{
+    $user = $this->where('email', $email)->first();
+    $user = $this->where('email', $email)->first();
+    if (!$user) {
+        return false;
+    }
+    
+    return app('hash')->check($password, $user->password);
+}
+    
 }
